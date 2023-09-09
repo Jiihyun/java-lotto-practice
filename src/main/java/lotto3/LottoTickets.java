db1cbb8b66ca278ff1c49f3d0b862e4626194d61
지현 <cwlgusc@gmail.com>
2023-09-09 23:34:03 +0900

    feat: first class collection lottoTickets

---
+++ src/main/java/lotto3/LottoTickets.java
@@ -0,0 +1,11 @@
+package lotto3;
+
+import java.util.List;
+
+public class LottoTickets {
+    private final List<LottoTicket> lottoTickets;
+
+  public LottoTickets(final List<LottoTicket> lottoTickets) {
+    this.lottoTickets = lottoTickets;
+  }
+}
