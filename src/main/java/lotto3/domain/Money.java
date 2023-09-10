6bdab578c901e2c9f5b9efc34327276aacfa79c9
지현 <cwlgusc@gmail.com>
2023-09-10 23:51:32 +0900

    feat: validate money

--- src/main/java/lotto3/domain/Money.java
+++ src/main/java/lotto3/domain/Money.java
@@ -14,4 +14,13 @@ public class Money {
     public void decrease(final int amount) {
         this.amount -= amount;
     }
+
+    //imp - 금액을 직접 변경하는 것이 아닌, Money 클래스에 돈을 증감시키는 메소드를 만들어서 간접적으로 변경하도록 할 것
+    // -> 한 번더 추상화 시키기
+    // buy할 때 마다 매번 검증하는 로직을 작성하지 않아도 됨
+    public void validateAmount(final int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("[ERROR] - 잔고가 부족합니다.");
+        }
+    }
 }
