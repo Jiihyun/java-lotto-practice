32da4a61409622d81a3dff292eea8039850d6c4e
지현 <cwlgusc@gmail.com>
2023-09-12 01:01:06 +0900

    feat: lotto drawing result(first collection)

---
+++ src/main/java/lotto3/domain/LottoDrawingResults.java
@@ -0,0 +1,11 @@
+package lotto3.domain;
+
+import java.util.List;
+
+public class LottoDrawingResults {
+    private final List<LottoDrawingResult> results;
+
+    public LottoDrawingResults(List<LottoDrawingResult> results) {
+        this.results = results;
+    }
+}
