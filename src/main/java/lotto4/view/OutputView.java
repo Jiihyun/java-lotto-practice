96c1cc3d45bd64adb41d74ec16f5dac438a46322
지현 <cwlgusc@gmail.com>
2023-09-14 17:05:26 +0900

    feat: outputView

--- src/main/java/lotto4/view/OutputView.java
+++ src/main/java/lotto4/view/OutputView.java
@@ -1,4 +1,27 @@
 package lotto4.view;
 
+import lotto4.util.MessageConst;
+
 public class OutputView {
+    public void printInputMoneyMsg() {
+        System.out.println(MessageConst.INPUT_MONEY_MSG);
+    }
+
+    public void printLottoTickets() {
+        //TODO : LottoTickets 받아와서 구매한 로또 넘버들 출력
+        System.out.println(+MessageConst.TICKET_AMOUNT_CONFIRM_MSG);
+    }
+
+    public void printInputWinningNumbers() {
+        System.out.println(MessageConst.INPUT_WINNING_NUM_MSG);
+    }
+
+    public void printInputBonusNumber() {
+        System.out.println(MessageConst.INPUT_BONUS_NUM_MSG);
+    }
+
+    public void printWinningStatistics() {
+        System.out.println(MessageConst.WINNING_STATISTICS_MSG);
+        //TODO: LottoDrawingResults
+    }
 }
