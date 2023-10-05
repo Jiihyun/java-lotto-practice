4ba7080c3abc1b9611fd3cfe99ffcd3b109e3d77
지현 <cwlgusc@gmail.com>
2023-10-05 23:11:15 +0900

    feat: lotto class and first class collection lottos

---
+++ src/main/java/lotto5/domain/Lottos.java
@@ -0,0 +1,11 @@
+package lotto5.domain;
+
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+}
