27a53a1c699457611a73ceff10b85a1a9770d6ef
지현 <cwlgusc@gmail.com>
2023-10-05 23:12:40 +0900

    feat: increase and decrease money with validation

---
+++ src/main/java/lotto5/domain/Money.java
@@ -0,0 +1,38 @@
+package lotto5.domain;
+
+public class Money {
+    private int amount;
+
+    public Money(int amount) {
+        validateInput(amount);
+        this.amount = amount;
+    }
+
+    public void increase(int amount) {
+        int nextAmount = this.amount + amount;
+        validateAmount(nextAmount);
+        this.amount = nextAmount;
+    }
+
+    public void decrease(int amount) {
+        int nextAmount = this.amount - amount;
+        validateAmount(nextAmount);
+        this.amount = nextAmount;
+    }
+
+    private static void validateInput(int amount) {
+        if (amount % 1_000 != 0) {
+            throw new IllegalArgumentException("로또 구입 금액은 1000원 단위 입니다.");
+        }
+    }
+
+    private void validateAmount(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("금액은 음수가 될 수 없습니다.");
+        }
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+}
